97df2e0589bac4661331766546dafd471e50ec36
Đỗ Trung Đức <duc010298@gmail.com>
2019-04-22 19:32:37 +0700

    Update database.sql

--- Database/database.sql
+++ Database/database.sql
@@ -1,38 +1,59 @@
 SET timezone TO 'UTC';
-create extension if not exists "uuid-ossp";
+CREATE extension IF NOT EXISTS "uuid-ossp";
 -- user manager
-create table if not exists app_user
+CREATE TABLE IF NOT EXISTS app_user
 (
-  user_id               uuid default uuid_generate_v4() primary key,
-  user_name             varchar(36)  not null unique,
-  encrypted_password    varchar(128) not null,
-  token_active_after    timestamp not null default now()
+	user_id		uuid DEFAULT uuid_generate_v4() PRIMARY KEY,
+	user_name 	varchar(36)	NOT NULL UNIQUE,
+	encrypted_password		varchar(128) NOT NULL,
+	token_active_after		timestamp NOT NULL DEFAULT now()
+)
+CREATE TABLE IF NOT EXISTS app_role
+(
+  role_id   bigint      NOT NULL PRIMARY KEY,
+  role_name varchar(30) NOT NULL
 );
-create table if not exists app_role
+CREATE TABLE IF NOT EXISTS user_role
 (
-  role_id   bigint      not null primary key,
-  role_name varchar(30) not null unique
-);
-create table if not exists user_role
-(
-  user_id uuid not null,
-  role_id bigint not null,
-  primary key (user_id, role_id),
-  constraint user_role_fk1 foreign key (user_id) references app_user (user_id),
-  constraint user_role_fk2 foreign key (role_id) references app_role (role_id)
+  user_id uuid NOT NULL,
+  role_id bigint NOT NULL,
+  PRIMARY KEY (user_id, role_id),
+  CONSTRAINT user_role_fk1 FOREIGN KEY (user_id) REFERENCES app_user (user_id),
+  CONSTRAINT user_role_fk2 FOREIGN KEY (role_id) REFERENCES app_role (role_id)
 );
 --
-insert into app_user (user_id, user_name, encrypted_password, token_active_after)
-values ('44260380-cc98-4296-acac-1d57b88b2535', 'duc010298', '$2a$10$hjBz774Yg4Fff44DYseK4.w4p27w2enR0W.QxSxlIXA.TcxS2bYV.', default);
+INSERT INTO app_user (user_id, user_name, encrypted_password, token_active_after)
+VALUES ('44260380-cc98-4296-acac-1d57b88b2535', 'duc010298', '$2a$10$hjBz774Yg4Fff44DYseK4.w4p27w2enR0W.QxSxlIXA.TcxS2bYV.', DEFAULT);
 --
-insert into app_role (role_id, role_name)
-values (1, 'ROLE_ADMIN');
+INSERT INTO app_role (role_id, role_name)
+VALUES (1, 'ROLE_ADMIN');
 
-insert into app_role (role_id, role_name)
-values (2, 'ROLE_MEMBER');
+INSERT INTO app_role (role_id, role_name)
+VALUES (2, 'ROLE_MEMBER');
 --
-insert into user_role
-values ('44260380-cc98-4296-acac-1d57b88b2535', 1);
+INSERT INTO user_role
+VALUES ('44260380-cc98-4296-acac-1d57b88b2535', 1);
 
-insert into user_role
-values ('44260380-cc98-4296-acac-1d57b88b2535', 2);
+INSERT INTO user_role
+VALUES ('44260380-cc98-4296-acac-1d57b88b2535', 2);
+--
+CREATE TABLE IF NOT EXISTS device
+(
+	imei char(15) PRIMARY KEY,
+	user_id uuid NOT null,
+	device_name varchar(100) NOT NULL,
+	is_online BOOLEAN,
+	last_online timestamp,
+	CONSTRAINT device_fk1 FOREIGN KEY (user_id) REFERENCES app_user (user_id)
+);
+
+CREATE TABLE IF NOT EXISTS location_history
+(
+	location_id 	uuid DEFAULT uuid_generate_v4() PRIMARY key,
+	imei 			char(15) NOT NULL,
+	time_tracking 	timestamp,
+	latitude 		float,
+	longitude		float,
+	CONSTRAINT location_history_fk1 FOREIGN KEY (imei) REFERENCES device (imei)
+);
+
