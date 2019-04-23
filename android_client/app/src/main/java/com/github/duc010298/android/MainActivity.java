9f5d70e925220e3b787c7c822eefbc385475356e
Đỗ Trung Đức <duc010298@gmail.com>
2019-04-23 18:08:09 +0700

    android check permission

--- android_client/app/src/main/java/com/github/duc010298/android/MainActivity.java
+++ android_client/app/src/main/java/com/github/duc010298/android/MainActivity.java
@@ -1,7 +1,13 @@
 package com.github.duc010298.android;
 
+import android.Manifest;
+import android.app.Activity;
 import android.content.Intent;
 import android.content.SharedPreferences;
+import android.content.pm.PackageManager;
+import android.os.Build;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -10,6 +16,8 @@ import android.widget.Toast;
 
 import com.github.duc010298.android.util.LoginTask;
 import com.github.duc010298.android.util.ServicesHelper;
+
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -21,12 +29,12 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        //TODO check permission here
+        checkPermission();
 
         connectView();
 
         String token = getTokenFromMemory();
-        if(token != null && !token.isEmpty()) {
+        if (token != null && !token.isEmpty()) {
             //TODO testToken here
 
             ServicesHelper servicesHelper = new ServicesHelper();
@@ -34,6 +42,34 @@ public class MainActivity extends AppCompatActivity {
 
             Intent intent = new Intent(this, LoginSuccessActivity.class);
             startActivity(intent);
+        }
+    }
+
+    private void checkPermission() {
+        String[] listPermission = new String[]{
+                android.Manifest.permission.INTERNET,
+                android.Manifest.permission.READ_PHONE_STATE,
+                android.Manifest.permission.ACCESS_NETWORK_STATE,
+                android.Manifest.permission.ACCESS_COARSE_LOCATION,
+                android.Manifest.permission.ACCESS_FINE_LOCATION};
+        boolean isHaveEnoughPermission = true;
+        if (ContextCompat.checkSelfPermission(this, Manifest.permission.INTERNET) != PackageManager.PERMISSION_GRANTED) {
+            isHaveEnoughPermission = false;
+        }
+        if (ContextCompat.checkSelfPermission(this, Manifest.permission.READ_PHONE_STATE) != PackageManager.PERMISSION_GRANTED) {
+            isHaveEnoughPermission = false;
+        }
+        if (ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_NETWORK_STATE) != PackageManager.PERMISSION_GRANTED) {
+            isHaveEnoughPermission = false;
+        }
+        if (ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            isHaveEnoughPermission = false;
+        }
+        if (ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            isHaveEnoughPermission = false;
+        }
+        if(!isHaveEnoughPermission) {
+            ActivityCompat.requestPermissions(this, listPermission, 1);
         }
     }
 
@@ -46,7 +82,7 @@ public class MainActivity extends AppCompatActivity {
         String username = txtUsername.getText().toString().trim().toLowerCase();
         String password = txtPassword.getText().toString().trim().toLowerCase();
 
-        if(username.isEmpty() || password.isEmpty()) {
+        if (username.isEmpty() || password.isEmpty()) {
             Toast.makeText(this, "Username and password cannot be empty", Toast.LENGTH_SHORT).show();
             return;
         }
@@ -59,5 +95,14 @@ public class MainActivity extends AppCompatActivity {
     private String getTokenFromMemory() {
         SharedPreferences pre = getSharedPreferences("SecretToken", MODE_PRIVATE);
         return pre.getString("token", null);
+    }
+
+    private void restartActivity(Activity activity) {
+        if (Build.VERSION.SDK_INT >= 11) {
+            activity.recreate();
+        } else {
+            activity.finish();
+            activity.startActivity(activity.getIntent());
+        }
     }
 }
