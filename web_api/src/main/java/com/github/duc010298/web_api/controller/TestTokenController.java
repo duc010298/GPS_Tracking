3dd47dd98184e0ace1dae692f9c1d9658b8fc644
Đỗ Trung Đức <duc010298@gmail.com>
2019-04-23 21:13:59 +0700

    Update TestTokenController.java

--- web_api/src/main/java/com/github/duc010298/web_api/controller/TestTokenController.java
+++ web_api/src/main/java/com/github/duc010298/web_api/controller/TestTokenController.java
@@ -1,6 +1,6 @@
 package com.github.duc010298.web_api.controller;
 
-import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -8,7 +8,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/TestToken")
 public class TestTokenController {
 	
-	@GetMapping
+	@PostMapping
 	public String TestToken() {
 		return "Success";
 	}
