885e2154ca13a56a146414c65d40e2be22def7b8
Đỗ Trung Đức <duc010298@gmail.com>
2019-04-24 22:02:46 +0700

    Update MyDatabaseHelper.java

--- android_client/app/src/main/java/com/github/duc010298/android/util/MyDatabaseHelper.java
+++ android_client/app/src/main/java/com/github/duc010298/android/util/MyDatabaseHelper.java
@@ -1,8 +1,11 @@
 package com.github.duc010298.android.util;
 
+import android.content.ContentValues;
 import android.content.Context;
+import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
+import android.location.Location;
 
 public class MyDatabaseHelper extends SQLiteOpenHelper {
     private static final String DATABASE_NAME = "GPS_Tracking";
@@ -14,12 +17,41 @@ public class MyDatabaseHelper extends SQLiteOpenHelper {
 
     @Override
     public void onCreate(SQLiteDatabase db) {
-        String query = "CREATE TABLE Location_History(time_tracking TEXT, latitude REAL, longitude REAL)";
+        String query = "CREATE TABLE Location_History(time_tracking INTEGER, latitude REAL, longitude REAL)";
         db.execSQL(query);
     }
 
     @Override
     public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
 
+    }
+
+    public void addLocationHistory(Location location) {
+        SQLiteDatabase db = this.getWritableDatabase();
+
+        ContentValues values = new ContentValues();
+        values.put("time_tracking", location.getTime());
+        values.put("latitude", location.getLatitude());
+        values.put("longitude", location.getLongitude());
+
+        db.insert("Location_History", null, values);
+        db.close();
+    }
+
+    public int getCount() {
+        String query = "SELECT * FROM Location_History";
+        SQLiteDatabase db = this.getReadableDatabase();
+        Cursor cursor = db.rawQuery(query, null);
+        int count = cursor.getCount();
+
+        cursor.close();
+        db.close();
+        return count;
+    }
+
+    public void cleanDatabase() {
+        SQLiteDatabase db = this.getWritableDatabase();
+        db.execSQL("DELETE FROM Location_History");
+        db.close();
     }
 }
