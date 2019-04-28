df2f4cb6086829657a5e0c7922f7ac47f5cdd9ce
Đỗ Trung Đức <duc010298@gmail.com>
2019-04-28 23:15:33 +0700

    .

--- web_api/src/main/java/com/github/duc010298/web_api/controller/WebSocketController.java
+++ web_api/src/main/java/com/github/duc010298/web_api/controller/WebSocketController.java
@@ -1,15 +1,13 @@
-package com.github.duc010298.web_api.controller.socket;
+package com.github.duc010298.web_api.controller;
 
 import org.springframework.messaging.handler.annotation.MessageMapping;
 import org.springframework.messaging.handler.annotation.Payload;
-import org.springframework.messaging.handler.annotation.SendTo;
 import org.springframework.stereotype.Controller;
 
 @Controller
 public class WebSocketController {
 	
 	@MessageMapping("/chat.sendMessage")
-    @SendTo("/topic/publicChatRoom")
     public String sendMessage(@Payload String chatMessage) {
         return chatMessage;
     }
