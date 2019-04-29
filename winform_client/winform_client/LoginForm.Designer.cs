88db6241756699f39cb488e7c4b214f69c1d6627
Đỗ Trung Đức <duc010298@gmail.com>
2019-04-29 21:37:03 +0700

    Update LoginForm.Designer.cs

--- winform_client/winform_client/LoginForm.Designer.cs
+++ winform_client/winform_client/LoginForm.Designer.cs
@@ -113,6 +113,7 @@
             this.Controls.Add(this.groupBox1);
             this.Controls.Add(this.label1);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MaximizeBox = false;
             this.Name = "LoginForm";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "GPS Tracking Manger - Login";
