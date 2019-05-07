58fba82ff53d4e421e8e19f134f8c08cfaee9a0c
Đỗ Trung Đức <duc010298@gmail.com>
2019-05-07 19:19:11 +0700

    Create GetCurrentLocationTask.java

---
+++ Android Version 2/Android/app/src/main/java/com/github/duc010298/gps_tracking/android/tasks/GetCurrentLocationTask.java
@@ -0,0 +1,81 @@
+package com.github.duc010298.gps_tracking.android.tasks;
+
+import android.Manifest;
+import android.content.Context;
+import android.content.pm.PackageManager;
+import android.location.Location;
+import android.os.Bundle;
+import android.os.Looper;
+import android.support.annotation.Nullable;
+import android.support.v4.app.ActivityCompat;
+
+import com.github.duc010298.gps_tracking.android.helper.DatabaseHelper;
+import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.location.FusedLocationProviderClient;
+import com.google.android.gms.location.LocationCallback;
+import com.google.android.gms.location.LocationRequest;
+import com.google.android.gms.location.LocationResult;
+import com.google.android.gms.location.LocationServices;
+
+public class GetCurrentLocationTask implements GoogleApiClient.ConnectionCallbacks {
+    private Context context;
+    private final long UPDATE_INTERVAL = 100;
+    private final long FASTEST_INTERVAL = 100;
+
+    private LocationRequest locationRequest;
+    private LocationCallback locationCallback;
+    private FusedLocationProviderClient fusedLocationClient;
+
+    private DatabaseHelper databaseHelper;
+
+    public GetCurrentLocationTask(final Context context) {
+        this.context = context;
+        databaseHelper = new DatabaseHelper(context);
+        fusedLocationClient = LocationServices.getFusedLocationProviderClient(context);
+        locationCallback = new LocationCallback() {
+            @Override
+            public void onLocationResult(LocationResult locationResult) {
+                if (locationResult == null) {
+                    return;
+                }
+                for (Location location : locationResult.getLocations()) {
+                    databaseHelper.addLocationHistory(location);
+                    SendLocationHistoryTask sendLocationHistoryTask = new SendLocationHistoryTask(context);
+                    sendLocationHistoryTask.execute();
+                    stopLocationUpdates();
+                }
+            }
+        };
+        buildLocationRequest();
+        startLocationUpdates();
+    }
+
+    @Override
+    public void onConnected(@Nullable Bundle bundle) {
+
+    }
+
+    @Override
+    public void onConnectionSuspended(int i) {
+
+    }
+
+    private void buildLocationRequest() {
+        locationRequest = LocationRequest.create();
+        locationRequest.setPriority(LocationRequest.PRIORITY_HIGH_ACCURACY);
+        locationRequest.setInterval(UPDATE_INTERVAL);
+        locationRequest.setFastestInterval(FASTEST_INTERVAL);
+    }
+
+    private void startLocationUpdates() {
+        if (ActivityCompat.checkSelfPermission(context, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED
+                && ActivityCompat.checkSelfPermission(context, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            return;
+        }
+        fusedLocationClient.requestLocationUpdates(locationRequest, locationCallback, Looper.getMainLooper() /* Looper */);
+    }
+
+    private void stopLocationUpdates() {
+        fusedLocationClient.removeLocationUpdates(locationCallback);
+    }
+}
