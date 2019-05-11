7af6d572236ae7a61a3a2f65f7a83dd240e6b384
Đỗ Trung Đức <duc010298@gmail.com>
2019-05-11 23:03:59 +0700

    .

---
+++ android_client/app/src/main/java/com/github/duc010298/gpstracking/activity/MainActivity.java
@@ -0,0 +1,156 @@
+package com.github.duc010298.gpstracking.activity;
+
+import android.Manifest;
+import android.app.Activity;
+import android.content.Intent;
+import android.content.IntentSender;
+import android.content.pm.PackageManager;
+import android.support.annotation.NonNull;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.github.duc010298.gpstracking.R;
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.GoogleApiAvailability;
+import com.google.android.gms.common.api.ResolvableApiException;
+import com.google.android.gms.location.LocationRequest;
+import com.google.android.gms.location.LocationServices;
+import com.google.android.gms.location.LocationSettingsRequest;
+import com.google.android.gms.location.LocationSettingsResponse;
+import com.google.android.gms.location.SettingsClient;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.Task;
+
+public class MainActivity extends AppCompatActivity {
+
+    private EditText txtUsername;
+    private EditText txtPassword;
+    private final long UPDATE_INTERVAL = 600000;
+    private final long FASTEST_INTERVAL = 600000;
+    private final int REQUEST_CHECK_SETTINGS = 1998;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        checkPermission();
+
+        if(!(GoogleApiAvailability.getInstance().isGooglePlayServicesAvailable(this) == ConnectionResult.SUCCESS)) {
+            Toast.makeText(this, getString(R.string.noti3), Toast.LENGTH_LONG).show();
+            finish();
+        }
+
+        checkLocationSetting();
+
+        connectView();
+    }
+
+    private void connectView() {
+        txtUsername = findViewById(R.id.txtUser);
+        txtPassword = findViewById(R.id.txtPass);
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, String[] permissions, int[] grantResults) {
+        if (requestCode == 1) {
+            for (int grantResult : grantResults) {
+                if (grantResult != PackageManager.PERMISSION_GRANTED) {
+                    Toast.makeText(this, getString(R.string.noti4), Toast.LENGTH_LONG).show();
+                    finish();
+                }
+            }
+        }
+    }
+
+    private void checkPermission() {
+        String[] listPermission = new String[]{
+                android.Manifest.permission.INTERNET,
+                android.Manifest.permission.READ_PHONE_STATE,
+                android.Manifest.permission.ACCESS_NETWORK_STATE,
+                android.Manifest.permission.ACCESS_COARSE_LOCATION,
+                android.Manifest.permission.ACCESS_FINE_LOCATION,
+                android.Manifest.permission.RECEIVE_BOOT_COMPLETED};
+        boolean isHaveEnoughPermission = true;
+        if (ContextCompat.checkSelfPermission(this, Manifest.permission.INTERNET) != PackageManager.PERMISSION_GRANTED) {
+            isHaveEnoughPermission = false;
+        }
+        if (ContextCompat.checkSelfPermission(this, Manifest.permission.READ_PHONE_STATE) != PackageManager.PERMISSION_GRANTED) {
+            isHaveEnoughPermission = false;
+        }
+        if (ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_NETWORK_STATE) != PackageManager.PERMISSION_GRANTED) {
+            isHaveEnoughPermission = false;
+        }
+        if (ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            isHaveEnoughPermission = false;
+        }
+        if (ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            isHaveEnoughPermission = false;
+        }
+        if (ContextCompat.checkSelfPermission(this, Manifest.permission.RECEIVE_BOOT_COMPLETED) != PackageManager.PERMISSION_GRANTED) {
+            isHaveEnoughPermission = false;
+        }
+        if (!isHaveEnoughPermission) {
+            ActivityCompat.requestPermissions(this, listPermission, 1);
+        }
+    }
+
+    private void checkLocationSetting() {
+        LocationRequest locationRequest = LocationRequest.create();
+        locationRequest.setPriority(LocationRequest.PRIORITY_HIGH_ACCURACY);
+        locationRequest.setInterval(UPDATE_INTERVAL);
+        locationRequest.setFastestInterval(FASTEST_INTERVAL);
+
+        LocationSettingsRequest.Builder builder = new LocationSettingsRequest.Builder()
+                .addLocationRequest(locationRequest);
+        SettingsClient client = LocationServices.getSettingsClient(this);
+        Task<LocationSettingsResponse> task = client.checkLocationSettings(builder.build());
+
+        task.addOnFailureListener(this, new OnFailureListener() {
+            @Override
+            public void onFailure(@NonNull Exception e) {
+                if (e instanceof ResolvableApiException) {
+                    // Location settings are not satisfied, but this can be fixed
+                    // by showing the user a dialog.
+                    try {
+                        // Show the dialog by calling startResolutionForResult(),
+                        // and check the result in onActivityResult().
+                        ResolvableApiException resolvable = (ResolvableApiException) e;
+                        resolvable.startResolutionForResult(MainActivity.this, REQUEST_CHECK_SETTINGS);
+                    } catch (IntentSender.SendIntentException sendEx) {
+                        // Ignore the error.
+                    }
+                }
+            }
+        });
+    }
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if(requestCode == REQUEST_CHECK_SETTINGS) {
+            if(resultCode != Activity.RESULT_OK) {
+                Toast.makeText(this, getString(R.string.noti2), Toast.LENGTH_LONG).show();
+                finish();
+            }
+        }
+    }
+
+    public void doLogin(View view) {
+        String username = txtUsername.getText().toString().trim().toLowerCase();
+        String password = txtPassword.getText().toString().trim().toLowerCase();
+
+        if (username.isEmpty() || password.isEmpty()) {
+            Toast.makeText(this, getString(R.string.noti1), Toast.LENGTH_SHORT).show();
+            return;
+        }
+
+//        LoginTask loginTask = new LoginTask(this);
+//        loginTask.execute(username, password);
+    }
+}
